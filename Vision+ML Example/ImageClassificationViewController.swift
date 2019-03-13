15a69b8292d8751b0002f508734b45553164a7e5
Rogier van den Brink <rogier.vandenbrink@incentro.com>
2019-03-13 21:42:24 +0100

    Initial Commit

---
+++ Vision+ML Example/ImageClassificationViewController.swift
@@ -0,0 +1,37 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+View controller for applying Vision + Core ML processing.
+*/
+
+import UIKit
+import CoreML
+import AVFoundation
+import Vision
+import ImageIO
+
+class ImageClassificationViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    // MARK: AVCApture session
+    
+    // MARK: Orientation
+    
+    
+    // MARK: - Image Classification
+    /// - Tag: MLModelSetup
+    
+    
+    /// - Tag: Process SampleBuffer
+    
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    
+}
+
+
